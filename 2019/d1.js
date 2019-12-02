ffb93c8261f61940a3da6f7c374906b4d17ffdc4
Jay Wolfe <john.wolfe@ascendum.com>
2019-12-02 09:57:15 -0500

    d2 part 1 complete

--- 2019/d1.js
+++ 2019/d1.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-function calcFuel(num) {
-  return Math.floor(num / 3) - 2;
-}
-
 function calcFuelFuel(num, total = 0) {
   const fuel = Math.floor(num / 3) - 2;
 
@@ -20,8 +16,6 @@ function calcFuelFuel(num, total = 0) {
 const totalFuel = fs.readFileSync(path.join(__dirname, './d1.txt'), 'utf8')
   .split('\n')
   .map(n => calcFuelFuel(n))
-  .reduce((a, b) => a + b, 0);
-
-
+  .reduce((a, b) => a + b);
 
 console.log(totalFuel)
