d0f02d4ec4a298683224f1e7efdee225eec824b2
Jay Wolfe <john.wolfe@ascendum.com>
2019-12-02 21:57:42 -0500

    slight cleanup day 2

--- 2019/d2.js
+++ 2019/d2.js
@@ -1,7 +1,7 @@
 const originalInput = '1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,5,19,23,1,6,23,27,1,27,10,31,1,31,5,35,2,10,35,39,1,9,39,43,1,43,5,47,1,47,6,51,2,51,6,55,1,13,55,59,2,6,59,63,1,63,5,67,2,10,67,71,1,9,71,75,1,75,13,79,1,10,79,83,2,83,13,87,1,87,6,91,1,5,91,95,2,95,9,99,1,5,99,103,1,103,6,107,2,107,13,111,1,111,10,115,2,10,115,119,1,9,119,123,1,123,9,127,1,13,127,131,2,10,131,135,1,135,5,139,1,2,139,143,1,143,5,0,99,2,0,14,0'.split(',').map(n => +n);
 
 function part1(replacement1, replacement2) {
-  const input = [...originalInput];
+  const input = originalInput.slice();
   input[1] = replacement1;
   input[2] = replacement2;
 
@@ -27,15 +27,12 @@ console.log(part1(12, 2))
 
 
 function part2() {
-  const xPossibles = new Array(100).fill(0).map((n, i) => i);
-  const yPossibles = new Array(100).fill(0).map((n, i) => i);
-
-  for (const x of xPossibles) {
-    for (const y of yPossibles) {
-      const result = part1(x, y);
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      const result = part1(noun, verb);
 
       if (result === 19690720) {
-        return { x, y, result }
+        return 100 * noun + verb;
       }
     }
   }
